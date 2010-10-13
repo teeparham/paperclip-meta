e0175ab5080bd4ccb12fedf13d80f58a06e7e1da
Richard Strand <richard@richard-laptop.(none)>
2010-10-13 17:19:21 +0100

    Add ability to store width/height/size in their own columns

--- lib/paperclip-meta.rb
+++ lib/paperclip-meta.rb
@@ -4,17 +4,30 @@ module Paperclip
 
     # If model has #{name}_meta column we getting sizes of processed
     # thumbnails and saving it to #{name}_meta column.
+    #
+    # Also save the value in #{name}[_#{style}]_#{method} if it exists
+    #  (method is width|height|size)
     def post_process_styles
       original_post_process_styles
+      
+      meta = {}
 
-      if instance.respond_to?(:"#{name}_meta=")
-        meta = {}
-
-        @queued_for_write.each do |style, file|
-          geo = Geometry.from_file file
-          meta[style] = {:width => geo.width.to_i, :height => geo.height.to_i, :size => File.size(file) }
+      @queued_for_write.each do |style, file|
+        meta[style] = {}
+        [:width, :height, :size].each do |meth|
+          if (value = get_meta_from_file(file, meth))
+            meta[style][meth] = value
+            if instance.respond_to?(:"#{name}_#{style}_#{meth}=")
+              instance_write(:"#{style}_#{meth}", value)
+            end
+            if style == default_style && instance.respond_to?(:"#{name}_#{meth}=")
+              instance_write(:"#{meth}", value)
+            end
+          end
         end
-
+      end
+      
+      if instance.respond_to?(:"#{name}_meta=")
         instance_write(:meta, ActiveSupport::Base64.encode64(Marshal.dump(meta)))
       end
     end
@@ -33,10 +46,27 @@ module Paperclip
 
     private
     def meta_read(style, item)
-      if instance.respond_to?(:"#{name}_meta") && instance_read(:meta)
+      if instance.respond_to?(:"#{name}_#{style}_#{item}") && instance_read(:"#{style}_#{item}")
+        instance_read(:"#{style}_#{item}")
+      elsif style == default_style && instance.respond_to?(:"#{name}_#{item}") && instance_read(:"#{item}")
+        instance_read(:"#{item}")
+      elsif instance.respond_to?(:"#{name}_meta") && instance_read(:meta)
         if meta = Marshal.load(ActiveSupport::Base64.decode64(instance_read(:meta)))
           meta.key?(style) ? meta[style][item] : nil
         end
+      end
+    end
+    
+    def image?
+      !content_type.nil? and !!content_type.match(%r{\Aimage/})
+    end
+    
+    def get_meta_from_file(file, method)
+      case method
+        when :size then File.size(file)
+        when :width then image? ? Geometry.from_file(file).width.to_i : nil
+        when :height then image? ? Geometry.from_file(file).height.to_i : nil
+        else nil
       end
     end
   end
