66457d02ae1caa8677253117647803641304dd81
Tee Parham <tee@neighborland.com>
2014-01-30 14:57:20 -0700

    Code style

--- lib/paperclip-meta/attachment.rb
+++ lib/paperclip-meta/attachment.rb
@@ -11,31 +11,32 @@ module Paperclip
       module InstanceMethods
 
         def save_with_meta_data
-          if (not @queued_for_delete.empty?) and @queued_for_write.empty?
-            instance_write(:meta, meta_encode({})) if instance.respond_to?(:"#{name}_meta=")
+          if @queued_for_delete.any? && @queued_for_write.empty? && instance.respond_to?(:"#{name}_meta=")
+            instance_write(:meta, meta_encode({}))
           end
           save_without_meta_data
         end
 
         def post_process_styles_with_meta_data(*style_args)
           post_process_styles_without_meta_data(*style_args)
+          return unless instance.respond_to?(:"#{name}_meta=")
 
-          if instance.respond_to?(:"#{name}_meta=")
-            meta = {}
-            @queued_for_write.each do |style, file|
-              begin
-                geo = Geometry.from_file file
-                meta[style] = {:width => geo.width.to_i, :height => geo.height.to_i, :size => file.size }
-              rescue Paperclip::Errors::NotIdentifiedByImageMagickError => e
-                meta[style] = {}
-              end
-            end
-
-            unless meta == {}
-              instance.send("#{name}_meta=", meta_encode(meta))
-              instance.class.where(instance.class.primary_key => instance.id).update_all({ "#{name}_meta" => meta_encode(meta) })
+          meta = {}
+          @queued_for_write.each do |style, file|
+            begin
+              geo = Geometry.from_file file
+              meta[style] = {:width => geo.width.to_i, :height => geo.height.to_i, :size => file.size }
+            rescue Paperclip::Errors::NotIdentifiedByImageMagickError
+              meta[style] = {}
             end
           end
+
+          return if meta == {}
+
+          instance.send("#{name}_meta=", meta_encode(meta))
+          instance.class
+            .where(instance.class.primary_key => instance.id)
+            .update_all("#{name}_meta" => meta_encode(meta))
         end
 
         #Use meta info for style if required
@@ -62,7 +63,7 @@ module Paperclip
         # Returns meta data for given style
         def meta_read(style, item)
           if instance.respond_to?(:"#{name}_meta") && instance_read(:meta)
-            if meta = meta_decode(instance_read(:meta))
+            if (meta = meta_decode(instance_read(:meta)))
               meta.key?(style) ? meta[style][item] : nil
             end
           end
