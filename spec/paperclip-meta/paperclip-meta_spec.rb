804d81e0d171518e96a0e86340efda5f999f58fb
Tee Parham <tee@neighborland.com>
2014-01-30 20:39:32 -0700

    Cleanup, speedup spec
    * Lazy load fixtures as private methods instead of always creating and
    setting ivars

--- spec/paperclip-meta/paperclip-meta_spec.rb
+++ spec/paperclip-meta/paperclip-meta_spec.rb
@@ -1,32 +1,22 @@
 require 'spec_helper'
 
-describe "Geometry saver plugin" do
-  before(:each) do
-    small_path = File.join(File.dirname(__FILE__), '..', 'fixtures', 'small.png')
-    big_path = File.join(File.dirname(__FILE__), '..', 'fixtures', 'big.jpg')
-    not_path = File.join(File.dirname(__FILE__), '..', 'fixtures', 'big.zip')
-    @big_image = File.open(big_path)
-    @small_image = File.open(small_path)
-    @big_size = Paperclip::Geometry.from_file(big_path)
-    @small_size = Paperclip::Geometry.from_file(small_path)
-    @not_image = File.open(not_path)
-  end
-
+describe "Attachment" do
   it "saves image geometry for original image" do
     img = Image.new
-    img.small_image = @small_image
+    img.small_image = small_image
     img.save!
 
-    img.reload # Ensure that updates really saved to db
+    img.reload
 
-    img.small_image.width eq(@small_size.width)
-    img.small_image.height eq(@small_size.height)
+    geometry = small_geometry
+    img.small_image.width eq(geometry.width)
+    img.small_image.height eq(geometry.height)
   end
 
   it "saves geometry for styles" do
     img = Image.new
-    img.small_image = @small_image
-    img.big_image = @big_image
+    img.small_image = small_image
+    img.big_image = big_image
     img.save!
 
     img.big_image.width(:small).should == 100
@@ -35,9 +25,7 @@ describe "Geometry saver plugin" do
 
   describe 'file size' do
     before do
-      @image = Image.new
-      @image.big_image = @big_image
-      @image.save!
+      @image = Image.create(big_image: big_image)
     end
 
     it 'should save file size with meta data ' do
@@ -54,15 +42,10 @@ describe "Geometry saver plugin" do
     it 'should have access to original file size' do
       @image.big_image.size.should == 37042
     end
-
   end
 
   it "clears geometry fields when image is destroyed" do
-    img = Image.new
-    img.small_image = @small_image
-    img.big_image = @big_image
-    img.save!
-
+    img = Image.create(small_image: small_image, big_image: big_image)
     img.big_image.width(:small).should == 100
 
     img.big_image = nil
@@ -73,9 +56,30 @@ describe "Geometry saver plugin" do
 
   it "does not fails when file is not an image" do
     img = Image.new
-    img.small_image = @not_image
-    lambda { img.save! }.should_not raise_error
+    img.small_image = not_image
+    -> { img.save! }.should_not raise_error
     img.small_image.width(:small).should be_nil
   end
 
+  private
+
+  def small_path
+    File.join(File.dirname(__FILE__), '..', 'fixtures', 'small.png')
+  end
+
+  def small_image
+    File.open(small_path)
+  end
+
+  def small_geometry
+    Paperclip::Geometry.from_file(small_path)
+  end
+
+  def big_image
+    File.open(File.join(File.dirname(__FILE__), '..', 'fixtures', 'big.jpg'))
+  end
+
+  def not_image
+    File.open(File.join(File.dirname(__FILE__), '..', 'fixtures', 'big.zip'))
+  end
 end
