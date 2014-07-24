e153122c8075bc8faa24be4bc3755d29c514542c
Tee Parham <tee@neighborland.com>
2014-07-24 10:54:19 -0600

    Fix indentation

--- lib/paperclip-meta/railtie.rb
+++ lib/paperclip-meta/railtie.rb
@@ -1,23 +1,23 @@
 require 'paperclip-meta'
 
 module Paperclip
-    module Meta
-
-      if defined? ::Rails::Railtie
-        class Railtie < ::Rails::Railtie
-          initializer :paperclip_meta do
-            ActiveSupport.on_load :active_record do
-              Paperclip::Meta::Railtie.insert
-            end
+  module Meta
+    
+    if defined? ::Rails::Railtie
+      class Railtie < ::Rails::Railtie
+        initializer :paperclip_meta do
+          ActiveSupport.on_load :active_record do
+            Paperclip::Meta::Railtie.insert
           end
         end
       end
+    end
 
-      class Railtie
-        def self.insert
-          Paperclip::Attachment.send(:include, Paperclip::Meta::Attachment)
-        end
+    class Railtie
+      def self.insert
+        Paperclip::Attachment.send(:include, Paperclip::Meta::Attachment)
       end
+    end
 
   end
 end
